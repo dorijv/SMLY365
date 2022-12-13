501e0c2d45d2f679f1404cb23a4f9fa80bf25655
dorijv <hjv6@hi.is>
2022-12-13 14:52:51 +0000

    init

---
+++ db.js
@@ -0,0 +1,34 @@
+const Pool = require('pg').Pool
+require('dotenv').config()
+
+const {
+  DATABASE_URL: connectionString,
+  NODE_ENV: nodeEnv = 'development',
+} = process.env;
+
+if (!connectionString) {
+  console.error('Vantar DATABASE_URL');
+  process.exit(1);
+}
+
+const ssl = nodeEnv !== 'development' ? { rejectUnauthorized: false } : false;
+
+const pool = new Pool({ connectionString, ssl });
+
+pool.on('error', (err) => {
+  console.error('Unexpected error on idle client', err);
+  process.exit(-1);
+});
+
+const getUsers = (request, response) => {
+  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error
+    }
+    response.status(200).json(results.rows)
+  })
+}
+
+module.exports = {
+  getUsers,
+}

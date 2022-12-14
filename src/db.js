2f32fdf93dfcc8b833672e381987ce888baf43c6
birkirfreyrg <birkirfrey@msn.com>
2022-12-14 16:44:09 +0000

    Looks like sign up works. Login problems. TODO Look up in DATABASE and confirm User and Password exists.

--- src/db.js
+++ src/db.js
@@ -1,7 +1,28 @@
 import dotenv from 'dotenv';
 import pg from 'pg';
+import express from 'express';
+import mysql from 'mysql';
+const app = express()
 
 dotenv.config();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'username',
+  password: 'password',
+  database: 'database_name'
+})
+
+app.get('/', (req, res) => {
+  // Read the contents of your SQL file
+  const sql = fs.readFileSync('schema.sql').toString()
+
+  // Execute the SQL query
+  connection.query(sql, (error, results) => {
+    if (error) throw error
+    res.send('SQL file successfully executed')
+  })
+})
 
 const {
   DATABASE_URL: connectionString,
@@ -31,6 +52,21 @@ export async function getUsers(request, response) {
     response.status(200).json(results.rows)
   })
 }
+
+export async function createUser(username, password) {
+  const q = 'INSERT INTO users (username, password) VALUES ($1, $2)';
+  const values = [username, password];
+  try {
+    await query(q, values);
+  } catch (e) {
+    console.error('Error', e);
+    return 'error';
+  }
+  console.info("User Created");
+  findByUsername(username);
+  return null;
+}
+
 
 export async function query(q, v = []) {
   const client = await pool.connect();
--- src/router.js
+++ src/router.js
@@ -2,13 +2,13 @@ import express from 'express';
 import { catchErrors } from './utils.js';
 export const router = express.Router();
 import passport, { ensureLoggedIn } from './auth.js';
-import { findByUsername } from './db.js';
-
+import { findByUsername, createUser, getUsers} from './db.js';
+import { createPoolCluster } from 'mysql';
 router.use(express.static('public'));
 
 async function login(req, res) {
   if (req.isAuthenticated()) {
-    return res.redirect('/admin');
+    return res.redirect('/isauth');
   }
 
   let message = '';
@@ -30,8 +30,17 @@ router.get('/signup',(req, res) => {
 
 router.post('/signup',(req, res) => {
     const {username, password} = req.body
-    //quary('INSERT INTO users (username, password) VALUES (username, password)');
+    createUser(username, password);
     res.redirect('/login');
+  },
+);
+router.post('/login',(req, res) => {
+    const {username, password} = req.body
+    if(username == 'test' && password == 'test123') {
+      return res.redirect('/sucess');
+    }else{
+     return res.redirect('/tryagain');
+    }
   },
 );
 

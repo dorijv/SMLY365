acaf65bb6b58d302bde736ba9efe850fdd1e0377
birkirfreyrg <birkirfrey@msn.com>
2022-12-14 01:33:28 +0000

    Signup page, en náði ekki að finna út úr database.
    Vantar alla virkni að sign-a up. Fann ekki hvernig skal vinna með gagnagrunninn. Login virkar ekki heldur. Held áfram á morgun.

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { catchErrors } from './utils.js';
 export const router = express.Router();
 import passport, { ensureLoggedIn } from './auth.js';
+import { findByUsername } from './db.js';
 
 router.use(express.static('public'));
 
@@ -21,3 +22,16 @@ async function login(req, res) {
 }
 
 router.get('/login', catchErrors(login));
+
+router.get('/signup',(req, res) => {
+  let message = '';
+  res.render('signup', { title: 'Signup', message  });
+});
+
+router.post('/signup',(req, res) => {
+    const {username, password} = req.body
+    //quary('INSERT INTO users (username, password) VALUES (username, password)');
+    res.redirect('/login');
+  },
+);
+
